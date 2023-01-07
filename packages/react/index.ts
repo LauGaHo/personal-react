4324324935ff8b4e74a608ab97cc692a3181d4d8
刘嘉豪 <cattou147@icloud.com>
2023-01-07 16:57:32 +0800

    feat: init-jsx

--- packages/react/index.ts
+++ packages/react/index.ts
@@ -1,9 +1,8 @@
 // React
 
-import { jsx } from "./src/jsx";
-
+import { jsx } from './src/jsx';
 
 export default {
-  version: '0.0.0',
-  createElement: jsx,
+	version: '0.0.0',
+	createElement: jsx
 };
