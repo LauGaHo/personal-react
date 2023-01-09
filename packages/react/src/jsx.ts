b49d1cb7cda509ac3b6e607a35437924c432e8ea
刘嘉豪 <cattou147@icloud.com>
2023-01-09 13:19:19 +0800

    feat: rollup-config

--- packages/react/src/jsx.ts
+++ packages/react/src/jsx.ts
@@ -4,7 +4,7 @@ import {
 	Key,
 	Ref,
 	Props,
-	ReactElement,
+	ReactElementType,
 	ElementType
 } from 'shared/ReactTypes';
 
@@ -15,7 +15,7 @@ const ReactElement = function (
 	key: Key,
 	ref: Ref,
 	props: Props
-): ReactElement {
+): ReactElementType {
 	const element = {
 		$$typeof: REACT_ELEMENT_TYPE,
 		type,
--- packages/shared/ReactTypes.ts
+++ packages/shared/ReactTypes.ts
@@ -4,7 +4,7 @@ export type Ref = any;
 export type Props = any;
 export type ElementType = any;
 
-export interface ReactElement {
+export interface ReactElementType {
 	$$typeof: symbol | number;
 	type: ElementType;
 	key: Key;
