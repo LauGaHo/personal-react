bfdff4d02afc991c45989d88d5f497288bc386ed
刘嘉豪 <cattou147@icloud.com>
2023-01-15 02:09:49 +0800

    feat: add something about hooks

--- packages/react-reconciler/src/fiberHooks.ts
+++ packages/react-reconciler/src/fiberHooks.ts
@@ -1,9 +1,36 @@
+import internals from 'shared/internals';
 import { FiberNode } from './fiber';
 
+let currentlyRenderingFiber: FiberNode | null = null;
+let workInProgressHook: Hook | null = null;
+
+const { currentDispatcher } = internals;
+
+interface Hook {
+	memoizedState: any;
+	updateQueue: unknown;
+	next: Hook | null;
+}
+
 export function renderWithHooks(wip: FiberNode) {
+	// 执行赋值操作
+	currentlyRenderingFiber = wip;
+	wip.memoizedState = null;
+
+	const current = wip.alternate;
+
+	if (current !== null) {
+		// update
+	} else {
+		// mount
+		// currentDispatcher.current = ;
+	}
+
 	const Component = wip.type;
 	const props = wip.pendingProps;
 	const children = Component(props);
 
+	// 重置操作
+	currentlyRenderingFiber = null;
 	return children;
 }
--- packages/react/index.ts
+++ packages/react/index.ts
@@ -1,6 +1,18 @@
 // React
 
+import { Dispatcher, resolveDispatcher } from './src/currentDispatcher';
+import currentDispatcher from './src/currentDispatcher';
 import { jsxDEV } from './src/jsx';
+
+export const useState: Dispatcher['useState'] = (initialState) => {
+	const dispatcher = resolveDispatcher();
+	return dispatcher.useState(initialState);
+};
+
+// 内部数据共享层
+export const __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED = {
+	currentDispatcher
+};
 
 export default {
 	version: '0.0.0',
