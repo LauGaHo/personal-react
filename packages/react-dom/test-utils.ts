22c033878a07373d9aaae397ddfab4c5742b2ec1
刘嘉豪 <cattou147@icloud.com>
2023-01-19 15:59:59 +0800

    feat: add jest test config

--- packages/react-dom/test-utils.ts
+++ packages/react-dom/test-utils.ts
@@ -1,8 +1,9 @@
 import { ReactElementType } from 'shared/ReactTypes';
 // @ts-ignore
-import { createRoot } from 'react';
+import { createRoot } from 'react-dom';
 
-export function renderIntoContainer(element: ReactElementType) {
+export function renderIntoDocument(element: ReactElementType) {
 	const div = document.createElement('div');
-	createRoot(div).render(element);
+	// 返回值需要为 ReactElement 类型
+	return createRoot(div).render(element);
 }
--- packages/react/index.ts
+++ packages/react/index.ts
@@ -2,7 +2,7 @@
 
 import { Dispatcher, resolveDispatcher } from './src/currentDispatcher';
 import currentDispatcher from './src/currentDispatcher';
-import { jsxDEV } from './src/jsx';
+import { jsx, jsxDEV, isValidElement as isValidElementFn } from './src/jsx';
 
 export const useState: Dispatcher['useState'] = (initialState) => {
 	const dispatcher = resolveDispatcher();
@@ -14,7 +14,9 @@ export const __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED = {
 	currentDispatcher
 };
 
-export default {
-	version: '0.0.0',
-	createElement: jsxDEV
-};
+export const version = '0.0.0';
+
+// TODO 根据环境区分使用 jsx 还是 jsxDEV
+export const createElement = jsx;
+
+export const isValidElement = isValidElementFn;
